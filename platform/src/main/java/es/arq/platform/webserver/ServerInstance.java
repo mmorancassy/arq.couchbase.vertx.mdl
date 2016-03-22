e9c16520ad4581b7e9a2e72bb4d0c161a8e80cf1
mmoran <miguelmorancassy@gexterno.es>
2016-03-22 12:59:37 +0100

    Añadidos recursos estáticos en la fase de empaquetado en el pom

--- platform/src/main/java/es/arq/platform/webserver/ServerInstance.java
+++ platform/src/main/java/es/arq/platform/webserver/ServerInstance.java
@@ -2,12 +2,15 @@ package es.arq.platform.webserver;
 
 import io.vertx.core.Verticle;
 import io.vertx.core.Vertx;
-
+import io.vertx.core.impl.FileResolver;
 
 public class ServerInstance {
 
 	public static void main(String[] args) {
-		try {
+		try {			
+			// Disable vert.x file caching - only for develepment purposes!
+			System.setProperty(FileResolver.DISABLE_FILE_CACHING_PROP_NAME, "true");
+			
 			Verticle mainVerticle = new RoutingServer();
 			Vertx.vertx().deployVerticle(mainVerticle);
 			
