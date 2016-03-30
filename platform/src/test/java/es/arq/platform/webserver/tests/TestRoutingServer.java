33c1c3de74ac497125d224e4b6d9e6af9c414a0d
mmoran <miguelmorancassy@gexterno.es>
2016-03-30 16:48:37 +0200

    Añadido test de arranque del servidor

--- platform/src/test/java/es/arq/platform/webserver/tests/TestRoutingServer.java
+++ platform/src/test/java/es/arq/platform/webserver/tests/TestRoutingServer.java
@@ -1,12 +1,23 @@
 package es.arq.platform.webserver.tests;
 
-import org.junit.Test;
+import io.vertx.core.Verticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.impl.FileResolver;
+import es.arq.platform.webserver.RoutingServer;
 
 public class TestRoutingServer {
 
-	@Test
-	public void test() {
-
+	public static void main(String[] args) {
+		try {			
+			// Disable vert.x file caching - only for develepment purposes!
+			System.setProperty(FileResolver.DISABLE_FILE_CACHING_PROP_NAME, "true");
+			
+			Verticle mainVerticle = new RoutingServer();
+			Vertx.vertx().deployVerticle(mainVerticle);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
